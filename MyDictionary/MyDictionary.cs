9dcc1d3ce73c304b330e1048445fa651262ed5af
Görkem Aran <GrkemAran@hotmail.com>
2021-02-22 17:10:41 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<A,B>
+    {
+        A[] keys;//numbers
+        B[] values;//students
+        A[] tempKeys = new A[0];
+        B[] tempValues = new B[0];
+
+        public MyDictionary()
+        {
+            keys = new A[0];
+            values = new B[0];
+
+        }
+
+        public void Add(A item1,B item2)
+        {
+            Console.WriteLine(item1 + " numaralı öğrenci olan " + item2 + " sisteme eklendi.");
+        }
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //int for student id, string for student name
+            MyDictionary<int,string> students = new MyDictionary<int, string>();
+            students.Add(1, "Görkem");
+        }
+    }
+}
